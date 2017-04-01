55c94f41ba053fd5d133454ea2fcb147b4cab28c
jpinto7 <juan.pinto108@gmail.com>
2017-04-01 17:21:15 -0600

    Changed App path in index ios and android files

--- The Complete React Native and Redux Course/manager/index.android.js
+++ The Complete React Native and Redux Course/manager/index.android.js
@@ -1,10 +1,10 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+* Sample React Native App
+* https://github.com/facebook/react-native
+* @flow
+*/
 
 import { AppRegistry } from 'react-native';
-import App from './src';
+import App from './src/App';
 
 AppRegistry.registerComponent('manager', () => App);
