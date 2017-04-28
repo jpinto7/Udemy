1bc0b74c11c8a9adf3f9d19c51d7a7613828cfe3
Juan Pablo <Juan Pablo>
2017-04-28 17:15:02 -0600

    Added server component

--- Angular 4- The Complete Guide/my-first-app/src/app/app.module.ts
+++ Angular 4- The Complete Guide/my-first-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
