6987d790cc9fd8b739c394bac9a961c64d088580
Juan Pablo Pinto <juan.pinto108@gmail.com>
2017-07-26 12:14:00 -0600

    appDropdown directive was added

---
+++ Angular 4- The Complete Guide/course-project-app/src/app/shared/dropdown.directive.ts
@@ -0,0 +1,18 @@
+import {
+  Directive,
+  HostBinding,
+  HostListener
+} from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen: boolean = false;
+
+  constructor() {}
+
+  @HostListener('click') onClick() {
+    this.isOpen = !this.isOpen;
+  }
+}
